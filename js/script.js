a1b68109c0ac1cc2aa1ec8ef3dd87ae7941e4dc5
lucasrenandns <lucasrenandns96@gmail.com>
2023-11-18 15:28:07 -0300

    Updated project

--- js/script.js
+++ js/script.js
@@ -1,30 +1,34 @@
+// Modo estrito
 "use strict";
 
+// Função para gerar texto aleatório
 function randomText() {
     const text = "abcdefghijklmnopqrstuvwxyz0123456789";
     const letters = text[Math.floor(Math.random() * text.length)];
     return letters;
 }
 
+// Função para fazer "chover"
 function rain() {
     const cloud = document.querySelector(".container .cloud");
-    const divElement = document.createElement("div");
-    const left = Math.floor(Math.random() * 310)
-    const fontSize = Math.random() * 1.5;
+    const div = document.createElement("div");
+    const left = Math.floor(Math.random() * 310);
+    const size = Math.random() * 1.5;
     const duration = Math.random() * 1; 
 
-    divElement.classList.add("text");
-    cloud.appendChild(divElement);
-    divElement.innerText = randomText();
-    divElement.style.left = left + "px";
-    divElement.style.fontSize = 0.5 + fontSize + "em";
-    divElement.style.animationDuration = 1 + duration + "s";
+    div.classList.add("text");
+    cloud.appendChild(div);
+    div.innerText = randomText();
+    div.style.left = left + "px";
+    div.style.fontSize = 0.5 + size + "em";
+    div.style.animationDuration = 1 + duration + "s";
     
     setTimeout(function() {
-        cloud.removeChild(divElement);
+        cloud.removeChild(div);
     }, 2000);
 }   
 
+// Controlando a quantidade de "chuva"
 setInterval(function() {
     rain();
 }, 20);
